2b27203b55817c084f10b998cf531c6057f6c5ff
Pratik6001 <128393826+Pratik6001@users.noreply.github.com>
2025-08-09 16:52:22 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { UserProvider } from "./context/userContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+
     <UserProvider>
       <App />
     </UserProvider>
-  </StrictMode>
+
 );
