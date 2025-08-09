ddf745697597e6ed2863c1585e479c7883ba9d46
Pratik6001 <128393826+Pratik6001@users.noreply.github.com>
2025-08-09 16:44:12 +0530

    Update auth.js

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const { generateAuthToken } = require("../middleware/authMiddleware");
 const bcrypt = require("bcryptjs");
 
